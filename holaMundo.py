1d0388ea166b51174dd15d831f1612e318f8e331
Cristian Castilla <cycastilla@unimagdalena.edu.co>
2026-08-23 21:21:12 -0500

    Hola mundo v2

--- holaMundo.py
+++ holaMundo.py
@@ -1,7 +1,4 @@
-print("hola mundo");
+print("hola mundo v2");
 
-<<<<<<< HEAD
+
 print("hello world")
-=======
-print("adios mundo")#genera el conflicto
->>>>>>> a9f1b09b04c5160b7e3275890d46de140fb6fa18
